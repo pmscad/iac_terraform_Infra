fdd8f8082f6eec2554278c36eb6d4bc37fc9d9fa
Vikramjeet Multani <38405022+pmscad@users.noreply.github.com>
2021-03-20 11:16:52 +0000

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  backend "remote" {
+    organization = "Qinetiq_Orion"
+
+    workspaces {
+      name = "LogR-Cherwell"
+    }
+  }
+}
+
+provider    "aws"   {
+    region  =   "us-west-2"
+}
+
+resource    "aws_instance"  "server-1" {
+    ami =   "ami-830c94e3"
+    instance_type   = "t2.nano"
+    tags    =   {
+        Name    =   "server"
+    }
+}
